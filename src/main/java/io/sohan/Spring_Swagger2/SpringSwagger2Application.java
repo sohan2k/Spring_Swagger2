131aa3f75759f19c8fd7792c0df74aea7c8a930d
Sohan Barman <barmansohan9@gmail.com>
2022-03-19 16:16:39 +0530

    add swagger2 and swagger-ui

--- src/main/java/io/sohan/Spring_Swagger2/SpringSwagger2Application.java
+++ src/main/java/io/sohan/Spring_Swagger2/SpringSwagger2Application.java
@@ -2,8 +2,10 @@ package io.sohan.Spring_Swagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringSwagger2Application {
 
 	public static void main(String[] args) {
